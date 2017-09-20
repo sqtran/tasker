e4f288f812dc4c30fa4a8a4e005f160ea5e6e62b
Bove Domenic (MGJ3VRJ) <MGJ3VRJ@UPS.COM>
2017-09-20 15:31:54 -0400

    lets try this on ocp

--- src/main/java/com/ups/ttg/hubx/tasker/TaskerRouter.java
+++ src/main/java/com/ups/ttg/hubx/tasker/TaskerRouter.java
@@ -1,5 +1,6 @@
 package com.ups.ttg.hubx.tasker;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,19 @@ public class TaskerRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
         from("timer:hello?period={{timer.period}}")
-                .transform(method("myBean", "saySomething"))
+                .to("direct:one");
+//                .multicast().to("direct:one", "direct:two");
+
+        from("direct:one")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                .to("{{configuratorUrl}}")
                 .to("stream:out");
+
+        from("direct:two")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                .to("http://www.yahoo.com");
 
     }
 
