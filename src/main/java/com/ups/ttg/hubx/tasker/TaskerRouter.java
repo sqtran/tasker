5f1cf1cc8b6b590e9d57ec59eaac8aa557cc6733
Bove Domenic (MGJ3VRJ) <MGJ3VRJ@UPS.COM>
2017-10-03 21:55:29 -0400

    finished all the routes

--- src/main/java/com/ups/ttg/hubx/tasker/TaskerRouter.java
+++ src/main/java/com/ups/ttg/hubx/tasker/TaskerRouter.java
@@ -2,8 +2,18 @@ package com.ups.ttg.hubx.tasker;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TaskerRouter extends RouteBuilder {
@@ -11,23 +21,49 @@ public class TaskerRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("timer:hello?period={{timer.period}}")
-                .to("direct:one");
-//                .multicast().to("direct:one", "direct:two");
+        from("quartz://crdTimer?cron={{crddata.period}}")
+                .to("direct:setHeaders")
+                .multicast().to("direct:CRDData.EARMO", "direct:CRDData.LCHKY");
 
-        from("direct:one")
+        from("quartz://tdsTimer?cron={{tds.period}}")
+                .to("direct:setHeaders")
+                .multicast().to("direct:TDS.EARMO", "direct:TDS.LCHKY");
+
+        from("quartz://seasTimer?cron={{seas.period}}")
+                .to("direct:setHeaders")
+                .multicast().to("direct:SEAS.EARMO", "direct:SEAS.LCHKY");
+
+        from("direct:CRDData.EARMO")
+                .to("{{crddata.earmo}}")
+                .log(LoggingLevel.INFO,"Response from CRDData.EARMO: ${body}");
+
+        from("direct:CRDData.LCHKY")
+                .to("{{crddata.lchky}}")
+                .log(LoggingLevel.INFO,"Response from CRDData.LCHKY: ${body}");
+
+        from("direct:TDS.EARMO")
+                .to("{{tds.earmo}}")
+                .log(LoggingLevel.INFO,"Response from TDS.EARMO: ${body}");
+
+        from("direct:TDS.LCHKY")
+                .to("{{tds.lchky}}")
+                .log(LoggingLevel.INFO,"Response from TDS.LCHKY: ${body}");
+
+        from("direct:SEAS.EARMO")
+                .to("{{seas.earmo}}")
+                .log(LoggingLevel.INFO,"Response from SEAS.EARMO: ${body}");
+
+        from("direct:SEAS.LCHKY")
+                .to("{{seas.lchky}}")
+                .log(LoggingLevel.INFO,"Response from SEAS.LCHKY: ${body}");
+
+        from("direct:setHeaders").routeId("setHeaders")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                .to("{{configuratorUrl}}")
-                .log(LoggingLevel.INFO, "Response from {{configuratorUrl}}: ${body}");
-              //.to("log:com.ups.ttg.hubx.tasker?level=INFO&message=Response from {{configuratorUrl}}: ${body}");
-
-//            .log(LoggingLevel.DEBUG, "Processing file ${file:name}").to("bean:foo");
-
-
-        // from("direct:two")
-        //         .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-        //         .to("http://www.yahoo.com");
+                .setHeader("Content-Type", constant("application/json"))
+                .setHeader("Authorization", constant("Basic UPSDummyUser:UPSPassword"))
+                .setHeader("UserID", constant("Openshift"))
+                .setHeader("MobileID", constant("Scheduler"))
+                .setHeader("Role", constant("admin"));
 
     }
-
 }
